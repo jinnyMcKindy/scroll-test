767fb1c3ce882c7eda6c4085ac8819550a577141
Yulia <>
2022-05-01 14:26:36 +0300

    init

---
+++ src/components/userSearch/UserCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "UserCard",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="less"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <UserSearch />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import UserSearch from "@/components/UserSearch.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    UserSearch,
   },
 };
 </script>
